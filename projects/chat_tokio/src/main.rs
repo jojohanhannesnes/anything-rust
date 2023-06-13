5cc7b85005037f24852fb9d1ef9b0bf15bb13db1
johannes <johannes@johanness-MacBook-Pro.local>
2023-06-13 07:39:13 +0700

    chat tokiooooo

--- projects/chat_tokio/src/main.rs
+++ projects/chat_tokio/src/main.rs
@@ -1,4 +1,47 @@
+use std::collections::HashMap;
+
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let (tx, _rx) = broadcast::channel(10);
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+        tokio::spawn(async move {
+            let (read, mut write) = socket.split();
+
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
+
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            write.write_all(msg.as_bytes()).await.unwrap();
+                        }
+
+                    }
+                }
+            }
+        });
+    }
+}
+
+struct Counter<'a> {
+    words: HashMap<&'a str, usize>,
 }
