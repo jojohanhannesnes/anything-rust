3905972dacbcd7536eb37f7344eac1eec106319b
johannes <johannes@johanness-MacBook-Pro.local>
2023-06-23 15:48:34 +0700

    multithread rust book

---
+++ tutorials/multithread-web-server/src/lib.rs
@@ -0,0 +1,94 @@
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread::{self, JoinHandle},
+};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+pub struct Worker {
+    id: usize,
+    worker_thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv();
+            match job {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+        Worker {
+            id,
+            worker_thread: Some(thread),
+        }
+    }
+}
+#[derive(Debug)]
+pub struct PoolCreationError;
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.worker_thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    // pub fn new(size: usize) -> ThreadPool {
+    //     assert!(size > 0);
+    //     ThreadPool
+    // }
+
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size == 0 {
+            return Err(PoolCreationError);
+        }
+
+        let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        // multiple producer single consumer, cannot clone otherwise its multiple consumer, there will be race conditions
+        // use arc to pass things between threads, sharing ownerships
+        let receiver = Arc::new(Mutex::new(receiver));
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        Ok(ThreadPool {
+            workers,
+            sender: Some(sender),
+        })
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
